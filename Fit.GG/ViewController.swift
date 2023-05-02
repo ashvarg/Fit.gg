d03c220d049f257feb05eccad954cb2345353f9a
Ashwin George <ageo0015@student.monash.edu>
2023-05-02 23:48:28 +1000

    Initial Commit

---
+++ Fit.GG/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Fit.GG
+//
+//  Created by Ashwin George on 2/5/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
